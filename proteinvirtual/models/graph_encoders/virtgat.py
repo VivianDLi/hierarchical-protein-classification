3c2d2b8412390bd91fc5acd033f343ea09022be2
Linus Leong <linus.leong.yh@gmail.com>
2025-02-06 22:20:07 +0000

    Move adaptive_conv to model.utils

--- proteinvirtual/models/graph_encoders/virtgat.py
+++ proteinvirtual/models/graph_encoders/virtgat.py
@@ -11,47 +11,7 @@ from graphein.protein.tensor.data import ProteinBatch
 from torch_geometric.nn.resolver import normalization_resolver
 from proteinworkshop.types import EncoderOutput
 from proteinworkshop.models.utils import get_aggregation
-from proteinvirtual.models.utils import WeightedPool
-
-
-def adaptive_conv(conv,
-                  node_features,
-                  edge_features,
-                  supports_edge_weight: bool = False,
-                  supports_edge_attr: bool = False,
-                  supports_pos: bool = False):
-    if isinstance(node_features, tuple):
-        x = node_features[0].x, node_features[1].x
-        pos = getattr(node_features[0], "pos", None), getattr(node_features[1], "pos", None)
-    else:
-        x = node_features.x
-        pos = getattr(node_features, "pos", None)
-    edge_index = edge_features.edge_index
-    edge_weight = getattr(edge_features, "edge_weight", None)
-    edge_attr = getattr(edge_features, "edge_attr", None)
-
-    if supports_pos:
-        if supports_edge_weight and supports_edge_attr:
-            out = conv(x, edge_index, pos=pos, edge_weight=edge_weight,
-                        edge_attr=edge_attr)
-        elif supports_edge_weight:
-            out = conv(x, edge_index, pos=pos, edge_weight=edge_weight)
-        elif supports_edge_attr:
-            out = conv(x, edge_index, pos=pos, edge_attr=edge_attr)
-        else:
-            out = conv(x, edge_index, pos=pos)
-    else:
-        if supports_edge_weight and supports_edge_attr:
-            out = conv(x, edge_index, edge_weight=edge_weight,
-                        edge_attr=edge_attr)
-        elif supports_edge_weight:
-            out = conv(x, edge_index, edge_weight=edge_weight)
-        elif supports_edge_attr:
-            out = conv(x, edge_index, edge_attr=edge_attr)
-        else:
-            out = conv(x, edge_index)
-    return out
-
+from proteinvirtual.models.utils import WeightedPool, adaptive_conv
 
 
 class RealGAT(GAT):
--- proteinvirtual/models/utils.py
+++ proteinvirtual/models/utils.py
@@ -22,3 +22,41 @@ class WeightedPool(nn.Module):
         else:
             return out
 
+
+def adaptive_conv(conv,
+                  node_features,
+                  edge_features,
+                  supports_edge_weight: bool = False,
+                  supports_edge_attr: bool = False,
+                  supports_pos: bool = False):
+    if isinstance(node_features, tuple):
+        x = node_features[0].x, node_features[1].x
+        pos = getattr(node_features[0], "pos", None), getattr(node_features[1], "pos", None)
+    else:
+        x = node_features.x
+        pos = getattr(node_features, "pos", None)
+    edge_index = edge_features.edge_index
+    edge_weight = getattr(edge_features, "edge_weight", None)
+    edge_attr = getattr(edge_features, "edge_attr", None)
+
+    if supports_pos:
+        if supports_edge_weight and supports_edge_attr:
+            out = conv(x, edge_index, pos=pos, edge_weight=edge_weight,
+                        edge_attr=edge_attr)
+        elif supports_edge_weight:
+            out = conv(x, edge_index, pos=pos, edge_weight=edge_weight)
+        elif supports_edge_attr:
+            out = conv(x, edge_index, pos=pos, edge_attr=edge_attr)
+        else:
+            out = conv(x, edge_index, pos=pos)
+    else:
+        if supports_edge_weight and supports_edge_attr:
+            out = conv(x, edge_index, edge_weight=edge_weight,
+                        edge_attr=edge_attr)
+        elif supports_edge_weight:
+            out = conv(x, edge_index, edge_weight=edge_weight)
+        elif supports_edge_attr:
+            out = conv(x, edge_index, edge_attr=edge_attr)
+        else:
+            out = conv(x, edge_index)
+    return out
